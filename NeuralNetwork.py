225d398627e6efec7b8982314a344b9f7898e456
CS70 Dove <dashcroft@cs.hmc.edu>
2018-04-29 02:05:48 -0700

    Bugs fixed, NN solves XOR

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,9 +8,8 @@ class Layer():
         self.type = type
         self.prevNeurons = prevNeurons
         self.numNeurons = numNeurons
-        self.weights = np.matrix([[random.random()-.5 for j in range(prevNeurons)] for i in range(numNeurons)])
-        self.biases = np.matrix([random.random()-.5 for i in range(numNeurons)]).transpose()
-        print(self.weights)
+        self.weights = np.matrix([[random.random()*2-.5 for j in range(prevNeurons)] for i in range(numNeurons)])
+        self.biases = np.matrix([random.random()*2-.5 for i in range(numNeurons)]).transpose()
 
 class NeuralNetwork():
     def __init__(self, inputSize, learningRate, neurL):
@@ -53,13 +52,17 @@ class NeuralNetwork():
         error = expOutputs - outputsL[-1]
 
         for layerIndex in range(len(self.layers)-1, -1, -1):
-            gradient = self.learningRate * np.multiply(error, dsigFunc(outputsL[layerIndex]))
-            deltas = gradient * outputsL[layerIndex-1].transpose()
+            if layerIndex != len(self.layers)-1:
+                error = self.layers[layerIndex+1].weights.transpose() * error
+            gradient = np.multiply(error, dsigFunc(outputsL[layerIndex])) * self.learningRate
+            if layerIndex-1 >= 0:
+                deltas = gradient * outputsL[layerIndex-1].transpose()
+            else:
+                deltas = gradient * inputs.transpose()
             self.layers[layerIndex].weights += deltas
             self.layers[layerIndex].biases += gradient
-            error = self.layers[layerIndex].weights.transpose() * error
     
-    def show(self, NR):
+    def show(self, NR = 25):
         win = g.GraphWin("Neural Network", 800, 400)
         yPos = 2*NR
         for i in range(self.inputSize):
@@ -81,6 +84,8 @@ class NeuralNetwork():
                     t = g.Text(textLoc, str(round(layer.weights.item((i, j)), 3)))
                     t.draw(win)
                     prevY += 4*NR
+                biasText = g.Text(g.Point(xPos, yPos), str(round(layer.biases.item(i), 3)))
+                biasText.draw(win)
                 yPos += 4*NR
             yPos = 2*NR
             xPos += 10*NR
@@ -88,16 +93,16 @@ class NeuralNetwork():
         win.close()
 
 if __name__ == "__main__":
-    n = NeuralNetwork(2, .02, [4, 1])
+    n = NeuralNetwork(2, .1, [4, 1])
     examples = [([0, 0], [0]),
                 ([0, 1], [1]),
                 ([1, 0], [1]),
                 ([1, 1], [0])]
-    for x in range(4):
-        for i in range(3000):
+    for x in range(1):
+        for i in range(10000):
             n.train(random.choice(examples))
         print(n.feedForward([0, 0]))
         print(n.feedForward([0, 1]))
         print(n.feedForward([1, 0]))
         print(n.feedForward([1, 1]))
-        n.show(20)
+        n.show(25)
